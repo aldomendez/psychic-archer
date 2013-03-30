7d4e8d952f33608d2c585c3529684d70fa9c41bd
Aldo Mendez Reyes <aldomendez86@gmail.com>
2013-03-30 02:40:11 -0500

    Ya hace POST
    La incorporacion de las instrucciones necesarias para el manejo del POST
    ya fueron realizadas. Obtengo la pagina y la despliego en pantalla

--- webfetch.py
+++ webfetch.py
@@ -1,2 +1,7 @@
 import urllib.request, urllib.parse, urllib.error
-print(urllib.request.urlopen('http://google.com/').read())
+
+params = urllib.parse.urlencode({"txtFInicio":"2013-03-28","txtFFinal":"2013-03-29","optOpcion":"ambos","optgrouping":"dia"})
+params = params.encode('utf-8')
+# print(params)
+page = urllib.request.urlopen('http://mexico/reports/starts_outs.php',params).read()
+print(page)
