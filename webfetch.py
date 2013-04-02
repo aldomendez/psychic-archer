238e462283577f3a7816c01486a0407955dd3637
aldomendez86@gmail.com <aldomendez86@gmail.com>
2013-04-02 14:36:15 -0500

    REGEX OK!
    - Se remanufacturo todo el script para hacer funciones individuales y
    reutilizables.
    - Se remanufacturo la parte que busca las expresiones regulares. Ya
    retorna la cantidad de OUTs de un material determinado

--- webfetch.py
+++ webfetch.py
@@ -1,17 +1,56 @@
 import urllib.request, urllib.parse, urllib.error, re
-#f = open('starts_outs.txt', 'w')
-params = urllib.parse.urlencode({"txtFInicio":"2013-03-28","txtFFinal":"2013-03-29","optOpcion":"ambos","optgrouping":"dia"})
-params = params.encode('utf-8')
-# print(params)
-page = urllib.request.urlopen('http://mexico/reports/starts_outs.php',params).read()
-# print(page)
-decodedPage = page.decode('utf-8')
-# f.write(decodedPage)
-# f.close()
-pROSA = re.compile('0010536020; LR4 SHIM ROSA</TD><TD nowrap>LR4 SHIM ROSA</TD><TD nowrap>LR4</TD><TD nowrap>SFCREL</TD><TD nowrap>', re.MULTILINE)
-m = pROSA.match(decodedPage)
-if m:
-    print('Match found: ', m.group())
+def readFile(file):
+	f = open(file, 'r')
+	text = f.read()
+	f.close()
+	return text	
+
+def writeToFile(file,content):
+	try:
+		f = open(file,'w')
+		f.write(content)
+		f.close()
+	except :
+		raise
+	
+def readWeb(page,encodedParams):
+	page = urllib.request.urlopen(page,encodedParams).read()
+	return page.decode('utf-8')
+
+def encodeParams():
+	params = urllib.parse.urlencode({"txtFInicio":"2013-03-28","txtFFinal":"2013-03-29","optOpcion":"ambos","optgrouping":"dia"})
+	params = params.encode('utf-8')
+	return params
+
+def showResults(pattern):
+	m = pattern.search(page)
+	if m:
+			for result in pattern.finditer(page):
+				tag, mov, value = result.groups()
+				print('Match found: ',tag,mov,value)
+	else:
+		print('No match')
+
+def fetchResults(code):
+	regex = '(' + code + ').+(DONE).*>(\d+)'
+	print(regex)
+	pattern = re.compile(regex, re.MULTILINE)
+	showResults(pattern)	
+
+
+###################################
+# Si trabajo desde CyOptics sera True
+# Si quiero probar REGEX sera False
+# Si quiero hacer casi cualquier otra prueba sera False
+workingFromWeb = False
+
+if workingFromWeb:
+	params = encodeParams()
+	page = readWeb('http://mexico/reports/starts_outs.php',params)
 else:
-    print('No match')
-# print(decodedPage)
+	page = readFile('starts_outs.txt')
+
+fetchResults('1053602')
+fetchResults('1055605')
+
+
