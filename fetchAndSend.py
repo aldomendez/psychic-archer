0fd9509abfa7d2bcbc642140cee9e8bec7dac5fc
Aldo Mendez Reyes <aldomendez86@gmail.com>
2013-04-03 19:15:14 -0500

    Version Final
    Esta es la version que esta corriendo con un Scheduled Task en la
    fabrica

--- fetchAndSend.py
+++ fetchAndSend.py
@@ -20,7 +20,11 @@ def readWeb(page,encodedParams):
 	return page.decode('utf-8')
 
 def encodeParams():
-	params = urllib.parse.urlencode({"txtFInicio":"2013-03-28","txtFFinal":"2013-03-29","optOpcion":"ambos","optgrouping":"dia"})
+	global hoy
+	hoy = date.today()
+	ayer = hoy + timedelta(days = -1)
+	manana = hoy + timedelta(days=1)
+	params = urllib.parse.urlencode({"txtFInicio":str(hoy),"txtFFinal":str(manana),"optOpcion":"ambos","optgrouping":"dia"})
 	params = params.encode('utf-8')
 	return params
 
@@ -29,7 +33,7 @@ def getResults(pattern):
 	return m
 
 def parseREGEX(code):
-	regex = 'MZ.*(\d{4}-\d{2}-\d{2}).*(' + code + '.*)<.+(DONE).*>(\d+)'
+	regex = 'MZ.*(\d{4}-\d{2}-\d{2}).*(' + code + ').+(DONE).*>(\d+)'
 	pattern = re.compile(regex, re.MULTILINE)
 	return pattern	
 
@@ -37,10 +41,10 @@ def getDONES(code):
 	pattern = parseREGEX(code)
 	result = getResults(pattern)
 	if result:
-		print(result)
+		# print(result)
 		for r in result:
 			fecha, tag, mov, value = r
-			print(tag)
+			# print(tag)
 		fecha, tag, mov, value = result[0]
 		return ">> {:14s} {:10s} {:4s} {:4s}".format(tag,fecha,mov,value)
 	else:
@@ -57,14 +61,12 @@ def manageDates():
 	manana = hoy + timedelta(days=1)
 	#print(ayer,hoy,manana)
 
-manageDates()
-print(hoy)
 ###################################
 # Si trabajo desde CyOptics sera True
 # Si quiero probar REGEX sera False
 # Si quiero hacer casi cualquier otra prueba sera False
-workingFromWeb = False
-dones = ""
+workingFromWeb = True
+dones = str(date.today()) + "\n"
 if workingFromWeb:
 	params = encodeParams()
 	page = readWeb('http://mexico/reports/starts_outs.php',params)
@@ -72,25 +74,26 @@ else:
 	page = readFile('starts_outs.txt')
 
 #map(print, ['LR4 SHIM ROSA','LR4G1WALPS'])
-[appendDonesToMail(x) for x in ['LR4 SHIM ROSA','LR4G1WALPS','E2560S']]
+
+[appendDonesToMail(x) for x in ['LR4 SHIM ROSA','LR4G1WALPS']]
 print(dones)
 ####################################################
 # Configuracion del servicio
 
-# FROMADDR = "aldomendez86@gmail.com"
-# LOGIN    = "aldomendez86@gmail.com"
-# PASSWORD = "dsorokzerkmodtne"
-# TOADDRS  = ["aldomendez86@gmail.com"]
-# SUBJECT  = "OUTS LR4"
+FROMADDR = "aldomendez86@gmail.com"
+LOGIN    = "aldomendez86@gmail.com"
+PASSWORD = "dsorokzerkmodtne"
+TOADDRS  = ["amendez@cyoptics.com","tlugo@cyoptics.com"]
+SUBJECT  = "OUTS LR4"
  
-# msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n"
-#        % (FROMADDR, ", ".join(TOADDRS), SUBJECT) )
-# msg += "Dones de " + str(hoy) + "\n\n" + dones
+msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n"
+       % (FROMADDR, ", ".join(TOADDRS), SUBJECT) )
+msg += "Dones de " + str(hoy) + "\n\n" + dones
 
-# server = smtplib.SMTP('smtp.gmail.com', 587)
-# server.set_debuglevel(1)
-# server.ehlo()
-# server.starttls()
-# server.login(LOGIN, PASSWORD)
-# server.sendmail(FROMADDR, TOADDRS, msg)
-# server.quit()
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(LOGIN, PASSWORD)
+server.sendmail(FROMADDR, TOADDRS, msg)
+server.quit()
